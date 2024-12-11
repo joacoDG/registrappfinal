1ea12b93e855c84d457448d9809839f34598421e
Joaquin Allendes astorga <joaq.allendes@duocuc.clgit config --global user.name joacoDG>
2024-12-11 16:50:12 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'registrappfinal1',
+  webDir: 'www'
+};
+
+export default config;
